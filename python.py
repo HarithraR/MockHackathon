5789b2cff4dc87b9d532be0f0b810bedecdbb396
Harithra Rangarajan <HarithraR>
2025-07-15 21:32:42 +0530

    pushing cv sample

---
+++ python.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+print("hi")
+# create an black image with the square at the centre
+
+img = np.zeros((256, 256))
+
+for i in range(256):
+    img[:,i] = i
+plt.imshow(img, cmap="gray", vmin=0, vmax=255)
